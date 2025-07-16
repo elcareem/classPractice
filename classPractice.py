18bd47af9b1f0555cc5b9b13c8cf63124f0a8805
your_name <andrewmiracle100@gmail.com>
2025-07-16 11:30:01 +0100

    check bool values of numeric data

--- classPractice.py
+++ classPractice.py
@@ -1,6 +1,14 @@
+'''
 string1 = " "
 string2 = "new"
 string3 = ""
 string4 = ""
 
 print(bool(string1), bool(string2), bool(string3), bool(string4))
+'''
+
+num1 = 0.0
+num2 = 0
+num3 = 4
+num4 = 43.2
+print(bool(num1), bool(num2), bool(num3), bool(num4))
