21786e23506431e9c27002bc33ad267d57c00d2e
your_name <andrewmiracle100@gmail.com>
2025-07-16 15:39:11 +0100

    use shorthand operators for addition, subtraction, multiplication, division, floor division, exponentiation and modulus

--- classPractice.py
+++ classPractice.py
@@ -1,14 +1,27 @@
-'''
-string1 = " "
-string2 = "new"
-string3 = ""
-string4 = ""
+first_number = 3
+first_number += 3
+print("add:", first_number)
 
-print(bool(string1), bool(string2), bool(string3), bool(string4))
-'''
+first_number = 3
+first_number -= 3
+print("subtract:", first_number)
 
-num1 = 0.0
-num2 = 0
-num3 = 4
-num4 = 43.2
-print(bool(num1), bool(num2), bool(num3), bool(num4))
+first_number = 3
+first_number *= 3
+print("multiply:", first_number)
+
+first_number = 3
+first_number /= 3
+print("divide:", first_number)
+
+first_number = 3
+first_number //= 3
+print("floor divide:", first_number)
+
+first_number = 3
+first_number **= 3
+print("exponent:", first_number)
+
+first_number = 3
+first_number %= 3
+print("modulus:", first_number)
