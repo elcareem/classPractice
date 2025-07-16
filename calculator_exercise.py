a6933909cc5ad6a96b64d78904e9b971baa3cc0e
your_name <andrewmiracle100@gmail.com>
2025-07-16 13:05:10 +0100

    setup: include multiplication, division and exponentiation feature

--- calculator_exercise.py
+++ calculator_exercise.py
@@ -10,9 +10,21 @@ print("Enter two numbers to add")
 first_number = int(input("First Number: "))
 second_number = int(input("Second Number: "))
 sum = float(first_number + second_number)
-print(f"\nThe sum of {first_number} and {second_number} is {sum} \n")
+print(f"\nThe sum of {first_number} and {second_number} is {sum:.2f} \n")
 
-sub = first_number + second_number
+sub = first_number - second_number
 print("**************")
-print(f"The difference of {first_number} and {second_number} is {sub}")
-print(f"")
+print(f"The difference of {first_number} and {second_number} is {sub:.2f}\n")
+
+mul = first_number * second_number
+print("**************")
+print(f"The multiplication value of {first_number} and {second_number} is {mul:.2f}\n")
+
+div = first_number / second_number
+print("**************")
+print(f"The division value of {first_number} and {second_number} is {div:.2f}\n")
+
+exp = first_number ** second_number
+print("**************")
+print(f"The value of {first_number} raised to the power of {second_number} is {exp:.2f}\n")
+
